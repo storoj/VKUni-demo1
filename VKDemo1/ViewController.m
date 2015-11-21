1f735525d1075c17ee842ee6be9a4b502d303cf4
Aleksey Storozhev <dark.storoj@gmail.com>
2015-11-21 08:02:14 +0300

    load Tanya's photos

--- VKDemo1/ViewController.m
+++ VKDemo1/ViewController.m
@@ -7,15 +7,128 @@
 //
 
 #import "ViewController.h"
+#import <VKSdkFramework/VKSdk.h>
 
-@interface ViewController ()
+static NSString *const kCellIdentifier = @"Cell";
 
+@interface PhotoCell : UICollectionViewCell
+@property (nonatomic, strong) UIImageView *imageView;
+@end
+
+@implementation PhotoCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        _imageView = [[UIImageView alloc] initWithFrame:self.contentView.bounds];
+        _imageView.contentMode = UIViewContentModeScaleAspectFill;
+        _imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        _imageView.backgroundColor = [UIColor lightGrayColor];
+        [self.contentView addSubview:_imageView];
+    }
+    return self;
+}
+
+- (void)prepareForReuse {
+    [super prepareForReuse];
+    self.imageView.image = nil;
+}
+
+@end
+
+
+
+
+
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (nonatomic, strong) NSMutableArray *photos;
+@property (nonatomic, strong) UICollectionView *collectionView;
+
+@property (nonatomic, strong) NSNumber *loadingOffsetMarker;
 @end
 
 @implementation ViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        _photos = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)loadView {
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:[UICollectionViewFlowLayout new]];
+    collectionView.dataSource = self;
+    collectionView.delegate = self;
+    
+    [collectionView registerClass:[PhotoCell class] forCellWithReuseIdentifier:kCellIdentifier];
+    
+    self.view = collectionView;
+    self.collectionView = collectionView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self loadNextPhotos];
+}
+
+#pragma mark UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.photos count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    PhotoCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier forIndexPath:indexPath];
+    return cell;
+}
+
+#pragma mark UICollectionViewDelegate
+
+- (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item >= self.photos.count-10) {
+        [self loadNextPhotos];
+    }
+}
+
+#pragma mark Data Loading
+
+- (void)loadPhotosWithOffset:(NSInteger)offset count:(NSInteger)count {
+    if (self.loadingOffsetMarker != nil && [self.loadingOffsetMarker integerValue] == offset) {
+        return;
+    }
+    
+    NSLog(@"offset: %d", (int)offset);
+    
+    self.loadingOffsetMarker = @(offset);
+    
+    VKRequest *request = [[VKApi photos] prepareRequestWithMethodName:@"getUserPhotos"
+                                                        andParameters:@{
+                                                                        @"user_id" : @(34),
+                                                                        @"offset" : @(offset),
+                                                                        @"count" : @(count),
+                                                                        @"sort" : @(0),
+                                                                        @"photo_sizes" : @(1)
+                                                                        }
+                                                        andHttpMethod:@"POST"
+                                                      andClassOfModel:[VKPhotoArray class]];
+    
+    [request executeWithResultBlock:^(VKResponse *response) {
+        VKPhotoArray *photosArray = response.parsedModel;
+        [self.photos addObjectsFromArray:photosArray.items];
+        [self.collectionView reloadData];
+        
+        self.loadingOffsetMarker = nil;
+    } errorBlock:^(NSError *error) {
+        NSLog(@"Error: %@", error);
+        self.loadingOffsetMarker = nil;
+    }];
+}
+
+- (void)loadNextPhotos {
+    [self loadPhotosWithOffset:[self.photos count] count:20];
 }
 
 @end
